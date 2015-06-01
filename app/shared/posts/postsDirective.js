02ea6fbca4eea09d5c4c2c610ce97bff1254d81c
owen <obiddle@gmail.com>
2015-06-01 10:18:14 -0700

    {shared: posts} masonry grid layout

--- app/shared/posts/postsDirective.js
+++ app/shared/posts/postsDirective.js
@@ -5,9 +5,28 @@ simplySocialApp.directive('postsViewNavigator', [function () {
         restrict: 'EA',
         controller: function (postsService, $rootScope) {
 
+        	this.chunkPostData = function(){
+				data = {};
+				data.left = [];
+				data.middle = [];
+				data.right = [];
+				
+				for(index = 0; index < this.postData.length; index+= 3) {
+				  data.left.push(this.postData[index])
+				}	
+				for(index = 1; index < this.postData.length; index+= 3) {
+				  data.middle.push(this.postData[index])
+				}			
+				for(index = 2; index < this.postData.length; index+= 3) {
+				  data.right.push(this.postData[index])
+				}
+				return data;
+			}
+
 		    this.init = function(){
 		    	this.postType = "allListView";
 		    	this.postData = postsService.getPostData();	
+		    	this.postDataChunks = this.chunkPostData() // for masonry layout
 		    }
 		    this.init()
 
@@ -19,21 +38,41 @@ simplySocialApp.directive('postsViewNavigator', [function () {
 				this.postType = type;
 		    }
 
-            $rootScope.$on("update-post-data", function(e) {
-            	this.updatePostData();
-            });
-
 		    this.setBackgroundImage = function(image){
 		        return { 'background-image': 'url(' + image + ')' }
 		    }
 
-			this.openComments = function($index){
-			    this.postData[$index].toggleComment = true;
+			this.openComments = function(index){
+			    this.postData[index].toggleComment = true;
 			}
 
-			this.closeComments = function($index){
-			    this.postData[$index].toggleComment = false;
+			this.closeComments = function(index){
+			    this.postData[index].toggleComment = false;
 			}
+
+			this.openChunckComments = function(index, type){
+				if(type === 'left'){
+					this.postDataChunks.left[index].toggleComment = true;
+				} else if (type === 'middle'){
+					this.postDataChunks.middle[index].toggleComment = true;
+				} else {
+					this.postDataChunks.right[index].toggleComment = true;
+				}
+			}
+
+			this.closeChunkComments = function(index, type){
+				if(type === 'left'){
+					this.postDataChunks.left[index].toggleComment = false;
+				} else if (type === 'middle'){
+					this.postDataChunks.middle[index].toggleComment = false;
+				} else {
+					this.postDataChunks.right[index].toggleComment = false;
+				}
+			}
+
+            $rootScope.$on("update-post-data", function(e) {
+            	this.updatePostData();
+            });
 
         }
     }
@@ -63,3 +102,18 @@ simplySocialApp.directive("postsAllListTemplate", function() {
         }
     };
 });
+
+simplySocialApp.directive("postsAllGridTemplate", function() {
+    return {
+        restrict: "AE",
+        require: '^postsViewNavigator',
+        templateUrl: function(){
+            return "shared/posts/views/allGridView.html";  
+        },
+        link: function (scope, element, attrs, postsViewNavigatorCtrl) {
+            scope.postsViewNavigator = postsViewNavigatorCtrl;
+        }
+    };
+});
+
+
--- app/shared/posts/postsService.js
+++ app/shared/posts/postsService.js
@@ -62,68 +62,115 @@ var dataBlobPosts = {"allPosts": [
         "text" : "8 Apps to Turn Your Pipe Dreams into Prototypes on.mash.to/1oubyu8",
         "photo" : 0,
         "video" : 0,
-        "comments" : 0,
+        "comments" : [
+            {
+                "time" : "1h",
+                "user_name": "Meg Robichaud",
+                "avatar_image": "assets/images/post/dataBlobImages/avatar_image_02.jpg", 
+                "text": "Nice one!"
+            },
+            {
+                "time" : "2h",
+                "user_name": "Ren Walker",
+                "avatar_image": "assets/images/post/dataBlobImages/avatar_image_04.jpg",  
+                "text": "Reeling inspired now... thanks for the great article"
+            }
+        ],
         "user" : {
 	        "user_name": "Kerem Suer",
 	        "avatar_image": "assets/images/post/dataBlobImages/avatar_image_03.jpg", 
         }
        
+    },
+    {
+        "id": 04,
+        "time" : "1h",      
+        "text" : "How to get animations out of your head on.mash.to/1oubyu8 Funny and useful.",
+        "photo" : 0,
+        "video" : 0,
+        "comments" : [
+            {
+                "time" : "1h",
+                "user_name": "Meg Robichaud",
+                "avatar_image": "assets/images/post/dataBlobImages/avatar_image_02.jpg", 
+                "text": "Nice one!"
+            },
+            {
+                "time" : "2h",
+                "user_name": "Ren Walker",
+                "avatar_image": "assets/images/post/dataBlobImages/avatar_image_04.jpg",  
+                "text": "Reeling inspired now... thanks for the great article"
+            }
+        ],
+        "user" : {
+	        "user_name": "Ren Walker",
+	        "avatar_image": "assets/images/post/dataBlobImages/avatar_image_04.jpg", 
+        }
+       
+    },
+    {
+        "id": 05,
+        "time" : "1h",      
+        "text" : "Looking for something to read? Check this out on.mash.to/1oubyu8. If you don't like it then sue me. ",
+        "photo" : 0,
+        "video" : 0,
+        "comments" : 0,
+        "user" : {
+	        "user_name": "Liang Shi",
+	        "avatar_image": "assets/images/post/dataBlobImages/avatar_image_05.jpg", 
+        }
+       
+    },
+    {
+        "id": 06,
+        "time" : "1h",      
+        "text" : "You have to see this bike. It will make your daily commute an absolute joy ride! on.mash.to/1oubyu8",
+        "photo" : 0,
+        "video" : "assets/images/post/dataBlobImages/post_video_06.png",
+        "comments" : 0,
+        "user" : {
+	        "user_name": "Vitor Leal",
+	        "avatar_image": "assets/images/post/dataBlobImages/avatar_image_06.jpg", 
+        }
+       
+    },
+    {
+        "id": 07,
+        "time" : "50m",      
+        "text" : "Need some reading?? 11 free ebooks for designers | creative Bloq on.mash.to/1oubyu8 via @netmag",
+        "photo" : 0,
+        "video" : 0,
+        "comments" : [
+            {
+                "time" : "1h",
+                "user_name": "Kerem Suer",
+                "avatar_image": "assets/images/post/dataBlobImages/avatar_image_03.jpg", 
+                "text": "Great way to start the week. Thanks for sharing!"
+            },
+            {
+                "time" : "2h",
+                "user_name": "Vitor Leal",
+                "avatar_image": "assets/images/post/dataBlobImages/avatar_image_06.jpg",  
+                "text": "Reeling inspired now... thanks for the great article"
+            }
+        ],
+        "user" : {
+	        "user_name": "Pallavi Gupta",
+	        "avatar_image": "assets/images/post/dataBlobImages/avatar_image_07.jpg", 
+        }
+       
+    },
+    {
+        "id": 08,
+        "time" : "25m",      
+        "text" : "Solid Sunset here in San diego... instagram.com/p/mV0PUrHRwQ/",
+        "photo" : "assets/images/post/dataBlobImages/post_image_08.png",
+        "video" : 0,
+        "comments" : 0,
+        "user" : {
+	        "user_name": "Sam Soffes",
+	        "avatar_image": "assets/images/post/dataBlobImages/avatar_image_01.jpg", 
+        }
+       
     }
-    // {
-    // "id": 2,
-    // "time" : "1m":
-    // },
-    // {
-    // "id": 3,
-    // "time" : "1m":
-    // },
-    // {
-    // "id": 4,
-    // "time" : "1m":
-    // },
-    // {
-    // "id": 5,
-    // "time" : "1m":
-    // },
-    // {
-    // "id": 6,
-    // "time" : "1m":
-    // },
-    // {
-    // "id": 7,
-    // "time" : "1m":
-    // },
-    // {
-    // "id": 8,
-    // "time" : "1m":
-    // },
-    // {
-    // "id": 9,
-    // "time" : "1m":
-    // },
-    // {
-    // "id": 10,
-    // "time" : "1m":
-    // },
-    // {
-    // "id": 11,
-    // "time" : "1m":
-    // {
-    // "id": 12,
-    // "time" : "1m":
-    // },
-    // {
-    // "id": 13,
-    // "time" : "1m":
-    // },
-    // {
-    // "id": 14,
-    // "time" : "1m":
-    // },
-    // {
-    // "id": 15,
-    // "time" : "1m":
-    // },
-    // "id": 16,
-    // "time" : "1m":
 ]}
